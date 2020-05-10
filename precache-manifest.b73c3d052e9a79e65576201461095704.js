60bfc58b86ac79dbb405322171c654c9b1ff3ceb
Juan Pablo Senn <juanpsen@gmail.com>
2020-05-10 20:18:34 -0300

    Updates

---
+++ precache-manifest.b73c3d052e9a79e65576201461095704.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7318eb16ea472354827eb69ea5623e9c",
+    "url": "/index.html"
+  },
+  {
+    "revision": "b9899bf16a9e94b7a10d",
+    "url": "/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "3438bab6257f48d45679",
+    "url": "/static/js/2.5972c111.chunk.js"
+  },
+  {
+    "revision": "bdee34eeb0674c8417be314a3d09d422",
+    "url": "/static/js/2.5972c111.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b9899bf16a9e94b7a10d",
+    "url": "/static/js/main.ec42d28c.chunk.js"
+  },
+  {
+    "revision": "f020893961c7ac32e497",
+    "url": "/static/js/runtime-main.fb044e92.js"
+  }
+]);
